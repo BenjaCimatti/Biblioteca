242388088ee104974e50c14a0b5aaec18dd11d7c
BenjaCimatti <benja.cimatti@gmail.com>
2020-05-14 16:50:46 -0300

    Primer Commit

---
+++ Biblioteca/biblioteca/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+# Create your models here.
+
+class Autor(models.Model):
+    nombre = models.CharField(max_length=30)
+    codigo = models.AutoField(primary_key = True)
+    def __str__(self):
+        return str(self.nombre)
+
+class Libro(models.Model):
+    codigo = models.AutoField(primary_key = True)
+    titulo = models.CharField(max_length = 30)
+    editorial = models.CharField(max_length = 30)
+    paginas = models.IntegerField()
+    autor = models.ForeignKey(Autor, on_delete = models.CASCADE)
+    def __str__(self):
+        return str(self.titulo)
+
+class Ejemplar(models.Model):
+    codigo = models.AutoField(primary_key = True)
+    localizacion = models.CharField(max_length = 30)
+    libro = models.ForeignKey(Libro, on_delete = models.CASCADE)
+    def __str__(self):
+        return str('Ejemplar ' + self.codigo)
+
+class Usuario(models.Model):
+    telefono = models.IntegerField()
+    codigo = models.AutoField(primary_key = True)
+    nombre = models.CharField(max_length=30)
+    direccion = models.CharField(max_length = 30)
+    ejemplar = models.ManyToManyField(Ejemplar)
+    def __str__(self):
+        return str(self.nombre)
