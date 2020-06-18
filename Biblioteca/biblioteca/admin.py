c262cfd70fa4482ec9364e372d1ad7795257078c
BenjaCimatti <benja.cimatti@gmail.com>
2020-06-18 07:31:25 -0300

    Adicion de List Display

--- Biblioteca/biblioteca/admin.py
+++ Biblioteca/biblioteca/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
 from .models import *
+
+class AutorAdmin(admin.ModelAdmin):
+    list_display = ['nombre', 'codigo']
+
+class LibroAdmin(admin.ModelAdmin):
+    list_display = ['codigo', 'titulo', 'editorial', 'paginas', 'autor']
+
+class EjemplarAdmin(admin.ModelAdmin):
+    list_display = ['codigo', 'localizacion', 'libro']
+
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display = ['telefono', 'codigo', 'nombre', 'direccion']
 # Register your models here.
 
-admin.site.register(Autor)
-admin.site.register(Libro)
-admin.site.register(Ejemplar)
-admin.site.register(Usuario)
+admin.site.register(Autor, AutorAdmin)
+admin.site.register(Libro, LibroAdmin)
+admin.site.register(Ejemplar, EjemplarAdmin)
+admin.site.register(Usuario, UsuarioAdmin)
